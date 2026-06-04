b010da6b1150f33c46f7b8df057442d22c0602b5
error56 <46536532+error56@users.noreply.github.com>
2026-06-04 15:24:32 +0200

    Add `AcpiGetTable` to imported functions

--- acpica-sys/src/lib.rs
+++ acpica-sys/src/lib.rs
@@ -3,7 +3,7 @@
 #![allow(non_snake_case)]
 #![no_std]
 
-use core::ffi::c_void;
+use core::ffi::{c_char, c_void};
 use core::include;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
@@ -18,5 +18,6 @@ extern "C" {
     pub fn AcpiWalkNamespace(Type: ACPI_OBJECT_TYPE, StartObject: ACPI_HANDLE, MaxDepth: UINT32, DescendingCallback: ACPI_WALK_CALLBACK, AscendingCallback: ACPI_WALK_CALLBACK, Context: *mut c_void, ReturnValue: *mut *mut c_void) -> ACPI_STATUS;
     pub fn AcpiGetCurrentResources(Device: ACPI_HANDLE, RetBuffer: *mut ACPI_BUFFER) -> ACPI_STATUS;
     pub fn AcpiGetObjectInfo(Object: ACPI_HANDLE, ReturnBuffer: *mut *mut ACPI_DEVICE_INFO) -> ACPI_STATUS;
+    pub fn AcpiGetTable(Signature: *mut c_char, Instance: UINT32, OutTable: *mut *mut ACPI_TABLE_HEADER) -> ACPI_STATUS;
 }
 
